5649df970022c54445164a408918b20e3739cb44
mfiari <fiarimike@yahoo.fr>
2015-07-15 15:20:42 +0200

    modif

--- src/com/esgi/Client.java
+++ src/com/esgi/Client.java
@@ -25,7 +25,6 @@ public class Client {
 		message.question = question;
 		
 		server.receiveMessageFromClient(message.getMessageInByte(), this);
-		//displayResponse();
 	}
 
 	
--- src/com/esgi/Main.java
+++ src/com/esgi/Main.java
@@ -7,9 +7,11 @@ public class Main {
 		Server serverDNS = new Server(9142);
 		Client clientDNS1 = new Client(9025, serverDNS);
 		clientDNS1.getIpByDomainName("www.google.fr");
+		clientDNS1.getIpByDomainName("www.myges.com");
 		Client clientDNS2 = new Client(9025, serverDNS);
 		clientDNS2.getIpByDomainName("www.myges.com");
 		Client clientDNS3 = new Client(9025, serverDNS);
+		clientDNS3.getIpByDomainName("www.google.fr");
 		clientDNS3.getIpByDomainName("www.google.fr");
 	}
 
